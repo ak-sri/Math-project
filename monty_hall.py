4ba633556f68fcc3ee68a6921b91ff4a01435c14
ak-sri <137289527+ak-sri@users.noreply.github.com>
2024-03-08 16:05:49 +0530

    Update monty_hall.py

--- monty_hall.py
+++ monty_hall.py
@@ -11,10 +11,8 @@ while True:
     reveal = random.choice(doors)
     if reveal != car and reveal != chosen:
         break
-print(f"Behnd {reveal}, there is a goat.")
 
 # Get decision2 from user
-print(f"The first choice: {chosen}")
 
 #Avoiding chosen2 and chosen from being the same
 while True:
@@ -33,6 +31,6 @@ else:
 # Result
 print(f"The first choice: {chosen}" , f"The second choice: {chosen2}", sep='\n')
 if chosen2 == car:
-    print("You won the car! :)")
+    print("The computer won the car! :)")
 else:
-    print("You lost the car! :(")
+    print("The computer lost the car! :(")
