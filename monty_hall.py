b4a059258cd3c49cf53bf6f0f71864ea07d2256f
ak-sri <137289527+ak-sri@users.noreply.github.com>
2024-03-08 15:53:46 +0530

    Update monty_hall.py

--- monty_hall.py
+++ monty_hall.py
@@ -1,23 +1,26 @@
 import random
 
 doors = ["door1", "door2", "door3"]
+chosen = random.choice(doors)
 
 # Put a car in a door
 car = random.choice(doors)
 
-# Get decision1 from user
-decision1 = input("Choose a door: ")
-
 # Reveal a door
 while True:
     reveal = random.choice(doors)
-    if reveal != car and reveal != decision1:
+    if reveal != car and reveal != chosen:
         break
 print(f"Behnd {reveal}, there is a goat.")
 
 # Get decision2 from user
-print(f"Your first choice: {decision1}")
-decision2 = input("Choose a door again: ")
+print(f"The first choice: {chosen}")
+
+#Avoiding chosen2 and chosen from being the same
+while True:
+    chosen2 = random.choice(doors)
+    if chosen2 != chosen and chosen2 != reveal:
+        break
 
 # Reveal all door
 if car == "door1":
@@ -28,8 +31,8 @@ else:
     print("Door1: Goat, Door2: Goat, Door3: Car")
 
 # Result
-print(f"Your first choice: {decision1}" , f"Your second choice: {decision2}", sep='\n')
-if decision2 == car:
+print(f"The first choice: {chosen}" , f"The second choice: {chosen2}", sep='\n')
+if chosen2 == car:
     print("You won the car! :)")
 else:
     print("You lost the car! :(")
